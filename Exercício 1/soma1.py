923fcaeb11558812d984839e272761fc3d738b41
menger-economics <166852482+menger-economics@users.noreply.github.com>
2024-04-21 22:06:33 -0300

    Add files via upload

---
+++ Exercício 1/soma1.py
@@ -0,0 +1,24 @@
+"""
+Programa: "Imprime a soma de dois números"
+
+Descrição: Este programa imprime a soma de dois números
+Autor: Antônio Menger
+Data: 21/04/2024
+Versão: 0.0.1
+"""
+
+# 1. Alocação de memória
+
+numero_1=0
+numero_2=0
+soma=0
+
+# 2. Entrada de dados
+
+numero_1=float(input('Digite um número:\n'))
+numero_2=float(input('Digite um número:\n'))
+
+# 3. Saída de dados
+
+soma=a+b
+print('A soma de', numero_1, 'e', numero_2, 'é igual a', soma)
